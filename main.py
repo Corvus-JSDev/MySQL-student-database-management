bba344701aae40e6629192ee8f1913e3bf778c08
GiovanniS <giovanni.squillace.work@gmail.com>
2024-09-04 15:53:18 -0400

    add table columns

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from PyQt6.QtWidgets import QApplication, QVBoxLayout, QLabel, QWidget, QLineEdit, QPushButton, QMainWindow
+from PyQt6.QtWidgets import QApplication, QVBoxLayout, QLabel, QWidget, QLineEdit, QPushButton, QMainWindow, QTableWidget
 from PyQt6.QtGui import QAction
 import sys
 
@@ -11,10 +11,20 @@ class MainWindow(QMainWindow):
 		file_menu_item = self.menuBar().addMenu("&File")
 		help_menu_item = self.menuBar().addMenu("&Help")
 
-		# Add sub menu items
+		# Add sub-menu items
 		file_menu_item.addAction(QAction("Add Student", self))
 		help_menu_item.addAction(QAction("About", self))
 
+		# Add table
+		self.table = QTableWidget()
+		self.table.setColumnCount(4)
+		self.table.setHorizontalHeaderLabels(("ID", "Name", "Course", "Contact"))
+		self.setCentralWidget(self.table)
+
+
+	def load_data(self):
+		tabel = self.table
+		pass
 
 
 app = QApplication(sys.argv)
